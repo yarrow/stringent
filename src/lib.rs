515484a05573df9792a94d2fff6361f525d53119
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2020-02-05 13:59:20 -0500

    impl Error for CommandError
    Mostly Display, but `source` will give the underlying io::Error if
    spawning fails.
    
    I'm currently not printing the `io:Error` in the `SpawnFailed` case
    of `Display`, depending on the user to print from `source`.  I'm not
    sure that's correct.
    
    And I'm not sure tha `SpawnFailed` is always an accurate description.
    Is there any other way of getting an `io::Error`?

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@
 #![deny(clippy::all)]
 #![deny(clippy::pedantic)]
 
+use std::error::Error;
+use std::fmt;
 use std::io;
 
 #[derive(Debug)]
@@ -10,4 +12,27 @@ pub enum CommandError {
     SpawnFailed(io::Error),
     ExitCode(i32),
     Signal(Option<i32>),
+}
+
+impl fmt::Display for CommandError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use CommandError::*;
+        match self {
+            SpawnFailed(_) => write!(f, "Spawn failed"),
+            ExitCode(code) => write!(f, "Exit code {}", code),
+            Signal(signal) => match signal {
+                Some(sig) => write!(f, "Terminated by signal {}", sig),
+                None => write!(f, "Terminated"),
+            },
+        }
+    }
+}
+
+impl Error for CommandError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            CommandError::SpawnFailed(io_err) => Some(io_err),
+            _ => None,
+        }
+    }
 }

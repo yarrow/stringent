14787733d972d97ecf39bc14821f1f42e3697c6f
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2020-02-06 00:56:01 -0500

    Implement `Stringent` for the other `Result<>`s
    That is, `io::Result<Output>` and `io::Result<Option<ExitStatus>>`

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@
 use std::error::Error;
 use std::fmt;
 use std::io;
-use std::process::ExitStatus;
+use std::process::{ExitStatus, Output};
 use std::result::Result;
 
 #[derive(Debug)]
@@ -39,6 +39,25 @@ impl Error for CommandError {
     }
 }
 
+#[derive(Debug)]
+pub struct CommandErrorWithOutput {
+    err: CommandError,
+    stdout: Vec<u8>,
+    stderr: Vec<u8>,
+}
+
+impl fmt::Display for CommandErrorWithOutput {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.err.fmt(f)
+    }
+}
+
+impl Error for CommandErrorWithOutput {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        self.err.source()
+    }
+}
+
 pub trait Stringent<T, E> {
     fn stringent(self) -> Result<T, E>;
 }
@@ -49,28 +68,78 @@ fn signal_of(status: &ExitStatus) -> Option<i32> {
     status.signal()
 }
 
-// This handles the case where `code()==None` and `signal()==None`,
-// which as far as I know isn't possible.  But I don't know very far!
+// I don't think this can ever get called — it would mean that `code()`
+// on Windows returned None, which as far as I know isn't possible.
+// But I don't know very far!
 #[cfg(not(unix))]
 fn signal_of(status: &ExitStatus) -> Option<i32> {
     None
 }
 
+trait StringentResult
+where
+    Self: Sized,
+{
+    fn status(&self) -> Option<ExitStatus>;
+    fn stringent_result(self) -> Result<Self, CommandError> {
+        use CommandError::*;
+        match self.status() {
+            None => Ok(self),
+            Some(status) if status.success() => Ok(self),
+            Some(status) => match status.code() {
+                Some(code) => Err(ExitCode(code)),
+                None => Err(Signal(signal_of(&status))),
+            },
+        }
+    }
+}
+
+impl StringentResult for ExitStatus {
+    fn status(&self) -> Option<ExitStatus> {
+        Some(*self)
+    }
+}
+
+impl StringentResult for Option<ExitStatus> {
+    fn status(&self) -> Option<ExitStatus> {
+        *self
+    }
+}
+
 impl Stringent<ExitStatus, CommandError> for Result<ExitStatus, io::Error> {
     fn stringent(self) -> Result<ExitStatus, CommandError> {
-        use CommandError::*;
         match self {
-            Err(io_err) => Err(SpawnFailed(io_err)),
-            Ok(status) => {
-                if status.success() {
-                    Ok(status)
-                } else {
-                    match status.code() {
-                        Some(code) => Err(ExitCode(code)),
-                        None => Err(Signal(signal_of(&status))),
-                    }
-                }
-            }
+            Err(io_err) => Err(CommandError::SpawnFailed(io_err)),
+            Ok(status) => status.stringent_result(),
+        }
+    }
+}
+
+impl Stringent<Option<ExitStatus>, CommandError> for Result<Option<ExitStatus>, io::Error> {
+    fn stringent(self) -> Result<Option<ExitStatus>, CommandError> {
+        match self {
+            Err(io_err) => Err(CommandError::SpawnFailed(io_err)),
+            Ok(status) => status.stringent_result(),
+        }
+    }
+}
+
+impl Stringent<Output, CommandErrorWithOutput> for Result<Output, io::Error> {
+    fn stringent(self) -> Result<Output, CommandErrorWithOutput> {
+        match self {
+            Err(io_err) => Err(CommandErrorWithOutput {
+                err: CommandError::SpawnFailed(io_err),
+                stdout: vec![],
+                stderr: vec![],
+            }),
+            Ok(output) => match output.status.stringent_result() {
+                Err(err) => Err(CommandErrorWithOutput {
+                    err,
+                    stdout: output.stdout,
+                    stderr: output.stderr,
+                }),
+                Ok(_) => Ok(output),
+            },
         }
     }
 }

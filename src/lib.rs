1bb953477f9f224db1435b9459961bb1bdaa1238
Yarrow Angelweed <yarrow.angelweed@gmail.com>
2020-02-15 16:18:10 -0500

    struct CommandError, enum CommandStatusError
    Make (the newly-renamed) `CommandError` struct the default error type
    for `stringent`.
    
    It's a struct that's capable of holding `stdout` and `stderr`, while
    CommandStatusError is the `enum` that records the type of error
    (`SpawnFailed`, `ExitCode`, or `Signal`) and is the type of the `err`
    field of `CommandError`. We implement `From<CommandStatusError> for
    CommandError`, so a routine with a mix `.status().verify()` and
    `.output.verify()` calls, for instance, can have a `Result<Foo,
    CommandError>`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-//! Return an error when a [`std::process::Command`][Command] method
-//! successfully starts a process, but the process itself isn't successful.
+//! This library provides a convenient way to return an error when a
+//! [`std::process::Command`][Command] process starts successfully but exits with an error code
+//! or is killed by a signal.
 //!
 //! [Command]: https://doc.rust-lang.org/std/process/struct.Command.html
 //! [status]: https://doc.rust-lang.org/std/process/struct.Command.html#method.status
@@ -13,9 +14,27 @@
 //! [ioResult]: https://doc.rust-lang.org/std/io/type.Result.html
 //! [ExitStatus]: https://doc.rust-lang.org/std/process/struct.ExitStatus.html
 //! [Output]: https://doc.rust-lang.org/std/process/struct.Output.html
-//! [verify]: trait.Stringent.html#tymethod.verify
-//! [CommandError]: enum.CommandError.html
-//! [CommandErrorWithOutput]: struct.CommandErrorWithOutput.html
+//! [verify]: trait.Verify.html#tymethod.verify
+//! [CommandStatusError]: enum.CommandStatusError.html
+//! [CommandError]: struct.CommandError.html
+//!
+//! # Example
+//!
+//! ```no_run
+//! use std::process::Command;
+//! use stringent::{CommandError, Verify};
+//!
+//! fn run_commands(first: &mut Command, second: &mut Command) -> Result<(), CommandError> {
+//!     Command::new("immediate").status().verify()?;
+//!     let mut child = Command::new("run_in_parallel").spawn().verify()?;
+//!     long_running_computation();
+//!     child.wait().verify()?;
+//!     //let output = child.wait_with_output().verify()?;
+//!     //process(&output.stdout);
+//!     Ok(())
+//! }
+//! # fn long_running_computation() { }
+//! ```
 //!
 //! The standard library's [`Command`][Command] module's [`io::Result`][ioResult]
 //! values for command completion can sometimes feel like "the operation was a
@@ -42,71 +61,58 @@
 //! methods, and to [`Child`][Child]'s [`wait()`][wait], [`try_wait`][try_wait], and
 //! [`wait_with_output`][wait_with_output] methods. The [`verify()`][verify] method turns
 //! unsuccessful [`ExitStatus`][ExitStatus] values into errors, so the following will return
-//! [`CommandError`][CommandError]s for commands that don't successfully complete:
+//! [`CommandStatusError`][CommandStatusError]s for commands that don't successfully complete:
 //! * `cmd.status().verify()?`
 //! * `child.wait().verify()?`
 //! * `child.try_wait().verify()?`
 //!
 //! [`verify()`][verify] similarly turns an unsuccessful [`Output`][Output] into a
-//! [`CommandErrorWithOutput`][CommandErrorWithOutput].  The `stdout` and `stderr` fields of the
+//! [`CommandError`][CommandError].  The `stdout` and `stderr` fields of the
 //! [`Output`][Output] are saved in the corresponding fields of the
-//! [`CommandErrorWithOutput`][CommandErrorWithOutput].
+//! [`CommandError`][CommandError].
 //! * `cmd.output().verify()?`
 //! * `child.wait_with_output().verify()?`
 //!
-//! # Example
-//!
-//! ```no_run
-//! use std::process::Command;
-//! use stringent::{CommandError, Stringent};
-//!
-//! fn run_commands(first: &mut Command, second: &mut Command) -> Result<(), CommandError> {
-//!     first.status().verify()?;
-//!     second.status().verify()?;
-//!     Ok(())
-//! }
-//! ```
-//!
-//! Without [`verify()`][verify] (but with [`CommandError`][CommandError]), we'd need to
+//! Without [`verify()`][verify] (but with [`CommandStatusError`][CommandStatusError]), we'd need to
 //! write something like this:
 //!
 //! ```no_run
 //! # use std::process::Command;
-//! # use stringent::CommandError;
+//! # use stringent::CommandStatusError;
 //!
-//! fn run_commands(first: &mut Command, second: &mut Command) -> Result<(), CommandError> {
+//! fn run_commands(first: &mut Command, second: &mut Command) -> Result<(), CommandStatusError> {
 //!     let mut status = match first.status() {
 //!         Ok(status) => status,
-//!         Err(io_err) => return Err(CommandError::SpawnFailed(io_err)),
+//!         Err(io_err) => return Err(CommandStatusError::SpawnFailed(io_err)),
 //!     };
 //!     if status.success() {
 //!         status = match second.status() {
 //!             Ok(status) => status,
-//!             Err(io_err) => return Err(CommandError::SpawnFailed(io_err)),
+//!             Err(io_err) => return Err(CommandStatusError::SpawnFailed(io_err)),
 //!         }
 //!     }
 //!     if status.success() { return Ok(()) }
 //!     match status.code() {
-//!         Some(code) => Err(CommandError::ExitCode(code)),
+//!         Some(code) => Err(CommandStatusError::ExitCode(code)),
 //!         None => {
 //!             #[cfg(unix)]
 //!                 use std::os::unix::process::ExitStatusExt;
 //!                 let signal = status.signal();
 //!             #[cfg(not(unix))]
 //!                 let signal = None;
-//!             Err(CommandError::Signal(signal))
+//!             Err(CommandStatusError::Signal(signal))
 //!         }
 //!     }
 //! }
 //! ```
 //! The [`verify()`][verify] method also handles the result of the [`spawn`][spawn] method,
-//! for convenience when writing functions that return a `Result<T, CommandError>`:
+//! for convenience when writing functions that return a `Result<T, CommandStatusError>`:
 //!
 //! ```no_run
 //! use std::process::Command;
-//! use stringent::{CommandError, Stringent};
+//! use stringent::{CommandStatusError, Verify};
 //!
-//! fn run_commands() -> Result<(), CommandError> {
+//! fn run_commands() -> Result<(), CommandStatusError> {
 //!     let mut child = Command::new("mycommand").spawn().verify()?;
 //!     // do more things...
 //!     child.wait().verify()?;
@@ -127,7 +133,7 @@ use std::result::Result;
 
 /// Adds error cases for commands that exit with error codes or that are killed
 #[derive(Debug)]
-pub enum CommandError {
+pub enum CommandStatusError {
     /// Holds the [`io::Error`][ioError] when
     /// [`status()`](https://doc.rust-lang.org/std/process/struct.Command.html#method.status)
     /// or
@@ -140,9 +146,9 @@ pub enum CommandError {
     Signal(Option<i32>),
 }
 
-impl fmt::Display for CommandError {
+impl fmt::Display for CommandStatusError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        use CommandError::*;
+        use CommandStatusError::*;
         match self {
             SpawnFailed(io) => write!(f, "Spawn failed: {}", io),
             ExitCode(code) => write!(f, "Exit code {}", code),
@@ -154,36 +160,47 @@ impl fmt::Display for CommandError {
     }
 }
 
-impl Error for CommandError {
+impl Error for CommandStatusError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            CommandError::SpawnFailed(io_err) => Some(io_err),
+            CommandStatusError::SpawnFailed(io_err) => Some(io_err),
             _ => None,
         }
     }
 }
 
-/// Includes `stdout` and `stderr` fields for saved output as well as a [`CommandError`](struct.CommandErrorWithOutput.html) field.
-///
-/// Note that the `stdout` and `stderr` fields are always present, even when `err` is
-/// `SpawnFailed`. In that case they are zero-length and no allocation was done.
+/// Holds the captured `stdout` and `stderr` from a [`std::process::Output`][Output] value.
 #[derive(Debug)]
-pub struct CommandErrorWithOutput {
-    /// Match on the `err` field to distinguish between `SpawnFailed`, `ExitCode`, and `Signal`.
-    pub err: CommandError,
+pub struct Std {
     /// Saved `stdout` from the command.
     pub stdout: Vec<u8>,
     /// Saved `stderr` from the command.
     pub stderr: Vec<u8>,
 }
 
-impl fmt::Display for CommandErrorWithOutput {
+/// Adds an `output` field `Option<Std>` field for saved `stdout` and `stderr` fields for saved
+/// output to the [`CommandStatusError`](struct.CommandStatusError.html) `err` field. The `output`
+/// field is `None` when
+///
+/// * `err` is `SpawnFailed`, or
+/// * This `CommandError` was create from a `CommandStatusError` (i.e., from the result of
+/// `status().verify()`, `wait().verify()`, or `try_wait().verify()`.
+///
+#[derive(Debug)]
+pub struct CommandError {
+    /// Match on the `err` field to distinguish between `SpawnFailed`, `ExitCode`, and `Signal`.
+    pub err: CommandStatusError,
+    /// Saved `stdout` from the command.
+    pub output: Option<Std>,
+}
+
+impl fmt::Display for CommandError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         self.err.fmt(f)
     }
 }
 
-impl Error for CommandErrorWithOutput {
+impl Error for CommandError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         self.err.source()
     }
@@ -191,10 +208,10 @@ impl Error for CommandErrorWithOutput {
 
 /// Adds the `verify()` method to the `io::Result` values returned by `Command::status`,
 /// `Command::output`, `Child::wait`, `Child::try_wait`, and `Child::wait_with_output`.
-pub trait Stringent<T, E> {
+pub trait Verify<T, E> {
     /// Changes a `Result<T, io::Error>` value to `Result<T, CmdErr>` value, where
-    /// `CmdErr` is `CommandError` when `T` is `ExitStatus` or `Option<ExitStatus>`,
-    /// and `CommandErrorWithOutput` when `T` is `Output`.
+    /// `CmdErr` is `CommandStatusError` when `T` is `ExitStatus` or `Option<ExitStatus>`,
+    /// and `CommandError` when `T` is `Output`.
     ///
     fn verify(self) -> Result<T, E>;
 }
@@ -218,8 +235,8 @@ where
     Self: Copy,
 {
     fn option_status(self) -> Option<ExitStatus>;
-    fn stringent_result(self) -> Result<Self, CommandError> {
-        use CommandError::*;
+    fn stringent_result(self) -> Result<Self, CommandStatusError> {
+        use CommandStatusError::*;
         match self.option_status() {
             None => Ok(self),
             Some(status) if status.success() => Ok(self),
@@ -243,49 +260,59 @@ impl StringentResult for Option<ExitStatus> {
     }
 }
 
-impl Stringent<ExitStatus, CommandError> for Result<ExitStatus, io::Error> {
-    fn verify(self) -> Result<ExitStatus, CommandError> {
+impl Verify<ExitStatus, CommandStatusError> for Result<ExitStatus, io::Error> {
+    fn verify(self) -> Result<ExitStatus, CommandStatusError> {
         match self {
-            Err(io_err) => Err(CommandError::SpawnFailed(io_err)),
+            Err(io_err) => Err(CommandStatusError::SpawnFailed(io_err)),
             Ok(status) => status.stringent_result(),
         }
     }
 }
 
-impl Stringent<Option<ExitStatus>, CommandError> for Result<Option<ExitStatus>, io::Error> {
-    fn verify(self) -> Result<Option<ExitStatus>, CommandError> {
+impl Verify<Option<ExitStatus>, CommandStatusError> for Result<Option<ExitStatus>, io::Error> {
+    fn verify(self) -> Result<Option<ExitStatus>, CommandStatusError> {
         match self {
-            Err(io_err) => Err(CommandError::SpawnFailed(io_err)),
+            Err(io_err) => Err(CommandStatusError::SpawnFailed(io_err)),
             Ok(status) => status.stringent_result(),
         }
     }
 }
 
-impl Stringent<Child, CommandError> for Result<Child, io::Error> {
-    fn verify(self) -> Result<Child, CommandError> {
+impl Verify<Child, CommandStatusError> for Result<Child, io::Error> {
+    fn verify(self) -> Result<Child, CommandStatusError> {
         match self {
-            Err(io_err) => Err(CommandError::SpawnFailed(io_err)),
+            Err(io_err) => Err(CommandStatusError::SpawnFailed(io_err)),
             Ok(child) => Ok(child),
         }
     }
 }
 
-impl Stringent<Output, CommandErrorWithOutput> for Result<Output, io::Error> {
-    fn verify(self) -> Result<Output, CommandErrorWithOutput> {
+impl Verify<Output, CommandError> for Result<Output, io::Error> {
+    fn verify(self) -> Result<Output, CommandError> {
         match self {
-            Err(io_err) => Err(CommandErrorWithOutput {
-                err: CommandError::SpawnFailed(io_err),
-                stdout: Vec::with_capacity(0),
-                stderr: Vec::with_capacity(0),
+            Err(io_err) => Err(CommandError {
+                err: CommandStatusError::SpawnFailed(io_err),
+                output: None,
             }),
             Ok(output) => match output.status.stringent_result() {
-                Err(err) => Err(CommandErrorWithOutput {
+                Err(err) => Err(CommandError {
                     err,
-                    stdout: output.stdout,
-                    stderr: output.stderr,
+                    output: Some(Std {
+                        stdout: output.stdout,
+                        stderr: output.stderr,
+                    }),
                 }),
                 Ok(_) => Ok(output),
             },
+        }
+    }
+}
+
+impl From<CommandStatusError> for CommandError {
+    fn from(status_error: CommandStatusError) -> Self {
+        Self {
+            err: status_error,
+            output: None,
         }
     }
 }
--- tests/unix-tests.rs
+++ tests/unix-tests.rs
@@ -1,10 +1,9 @@
 #![deny(warnings, unused, clippy::all, clippy::pedantic)]
-
 #![cfg(unix)]
-use std::process::ExitStatus;
 use std::os::unix::process::ExitStatusExt;
+use std::process::ExitStatus;
 
-use stringent::{CommandError, Stringent};
+use stringent::{CommandStatusError, Verify};
 
 const XCPU: i32 = 24;
 const PANIC: i32 = 101;
@@ -61,7 +60,7 @@ fn test_panic() {
     if let Some(status) = panicked() {
         let result = Ok(status).verify();
         match result {
-            Err(CommandError::ExitCode(PANIC)) => {}
+            Err(CommandStatusError::ExitCode(PANIC)) => {}
             _ => panic!("Expected panic status to report a bad exit code"),
         }
     }
@@ -72,7 +71,7 @@ fn test_killed_by_signal() {
     if let Some(status) = killed_by_signal() {
         let result = Ok(status).verify();
         match result {
-            Err(CommandError::Signal(Some(XCPU))) => {}
+            Err(CommandStatusError::Signal(Some(XCPU))) => {}
             _ => panic!("Expected killed_by_signal to report XCPU"),
         }
     }
@@ -89,49 +88,52 @@ fn show_exit_status_examples() {
     );
 }
 
-use std::process::{Command, Stdio};
 use std::path::Path;
+use std::process::{Command, Stdio};
 
-#[test] fn nonexistent_command() {
+#[test]
+fn nonexistent_command() {
     let cmd = "/nonexistent_command";
-    if ! Path::new(cmd).is_file() {
+    if !Path::new(cmd).is_file() {
         match Command::new(cmd).status().verify() {
             Ok(_) => panic!("{} should not have succeeded", cmd),
-            Err(CommandError::SpawnFailed(_)) => {},
+            Err(CommandStatusError::SpawnFailed(_)) => {}
             Err(e) => panic!("Unexpected error ({}) in executing {}", e, cmd),
         }
     }
 }
 
-#[test] fn bad_arguments() {
+#[test]
+fn bad_arguments() {
     let cmd = "/bin/sleep";
     if Path::new(cmd).is_file() {
         match Command::new(cmd).stderr(Stdio::null()).status().verify() {
             Ok(_) => panic!("{} should not have succeeded", cmd),
-            Err(CommandError::ExitCode(_)) => {}, // Sleep with no arguments should complain
+            Err(CommandStatusError::ExitCode(_)) => {} // Sleep with no arguments should complain
             Err(e) => panic!("Unexpected error ({}) in executing {}", e, cmd),
         }
     }
 }
 
-#[test] fn bad_arguments_with_output() {
+#[test]
+fn bad_arguments_with_output() {
     let cmd = "/bin/sleep";
     if Path::new(cmd).is_file() {
         match Command::new(cmd).output().verify() {
             Ok(_) => panic!("{} should not have succeeded", cmd),
-            Err(output) => match output.err {
-                CommandError::ExitCode(_) => {
-                    if output.stderr.len() == 0 {
-                        panic!("Expected to capture {}'s stderr", cmd);
-                    }
+            Err(e) => match e.err {
+                CommandStatusError::ExitCode(_) => match e.output {
+                    Some(stringent::Std { stdout: _, stderr }) if stderr.len() > 0 => {}
+                    _ => panic!("Expected to capture {}'s stderr", cmd),
                 },
-                _ => panic!("Unexpected error ({}) in executing {}", output.err, cmd),
-            }
+                _ => panic!("Unexpected error ({}) in executing {}", e.err, cmd),
+            },
         }
     }
 }
 
-#[test] fn killed() {
+#[test]
+fn killed() {
     let cmd = "/bin/sleep";
     if Path::new(cmd).is_file() {
         let mut child = Command::new(cmd).arg("3").spawn().verify().expect(cmd);
@@ -145,13 +147,14 @@ use std::path::Path;
         };
         match result {
             Ok(_) => panic!("Killed command {} should not have succeeded", cmd),
-            Err(CommandError::Signal(_)) => {},
+            Err(CommandStatusError::Signal(_)) => {}
             Err(e) => panic!("Unexpected error ({}) in executing {}", e, cmd),
         }
     }
 }
 
-#[test] fn killed_with_output() {
+#[test]
+fn killed_with_output() {
     let cmd = "/bin/sleep";
     if Path::new(cmd).is_file() {
         let mut child = Command::new(cmd).arg("3").spawn().verify().expect(cmd);
@@ -159,9 +162,9 @@ use std::path::Path;
         match child.wait_with_output().verify() {
             Ok(_) => panic!("Killed command {} should not have succeeded", cmd),
             Err(output) => match output.err {
-                CommandError::Signal(_) => { },
+                CommandStatusError::Signal(_) => {}
                 _ => panic!("Unexpected error ({}) in executing {}", output.err, cmd),
-            }
+            },
         };
     }
 }
